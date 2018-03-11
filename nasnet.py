481aebd92374d8e920029d5ca63e644a08603dde
Erik Gärtner <erik@gartner.io>
2018-03-11 15:00:29 +0100

    Some modification that fixes dimensions, does not seem to follow papers design though

--- nasnet.py
+++ nasnet.py
@@ -31,19 +31,19 @@ def _max_layer(pool_size, strides=(1, 1)):
 
 def _create_reduction_cell(x, x_1, nbr_filters, weight_decay):
 
-    sep7x7 = _sep_layer(x_1, nbr_filters, (7, 7), weight_decay)
-    sep5x5 = _sep_layer(x, nbr_filters, (5, 5), weight_decay)
+    sep7x7 = _sep_layer(x_1, nbr_filters, (7, 7), weight_decay, strides=(2, 2))
+    sep5x5 = _sep_layer(x, nbr_filters, (5, 5), weight_decay, strides=(2, 2))
     y1 = Add()([sep7x7, sep5x5])
 
-    max3x3 = _max_layer((3, 3))(x)
-    sep7x7 = _sep_layer(x_1, nbr_filters, (7, 7), weight_decay)
+    max3x3 = _max_layer((3, 3), strides=(2, 2))(x)
+    sep7x7 = _sep_layer(x_1, nbr_filters, (7, 7), weight_decay, strides=(2, 2))
     y2 = Add()([max3x3, sep7x7])
 
-    avg3x3 = _avg_layer((3, 3))(x)
-    sep5x5 = _sep_layer(x_1, nbr_filters, (5, 5), weight_decay)
+    avg3x3 = _avg_layer((3, 3), strides=(2, 2))(x)
+    sep5x5 = _sep_layer(x_1, nbr_filters, (5, 5), weight_decay, strides=(2, 2))
     y3 = Add()([avg3x3, sep5x5])
 
-    max3x3 = _max_layer((3, 3))(x)
+    max3x3 = _max_layer((3, 3), strides=(2, 2))(x)
     sep3x3 = _sep_layer(y1, nbr_filters, (3, 3), weight_decay)
     z1 = Add()([max3x3, sep3x3])
 
@@ -51,6 +51,7 @@ def _create_reduction_cell(x, x_1, nbr_filters, weight_decay):
     z2 = Add()([avg3x3, y2])
 
     result = Concatenate(axis=-1)([z1, z2, y3])
+    result = _sep_layer(result, nbr_filters * 2, (1, 1), weight_decay)
     return result, int(result.shape[3])
 
 
@@ -74,6 +75,7 @@ def _create_normal_cell(x, x_1, nbr_filters, weight_decay):
     y5 = Add()([sep5x5, sep3x3])
 
     result = Concatenate(axis=-1)([y1, y2, y3, y4, y5])
+    result = _sep_layer(result, nbr_filters, (1, 1), weight_decay)
     return result, int(result.shape[3])
 
 
@@ -86,7 +88,7 @@ def create_nasnet(input_shape, nbr_normal_cells, nbr_blocks, weight_decay, nbr_c
 
     ipt = Input(input_shape)
     # Sample up to nbr_filters of input
-    x = ipt
+    x = _sep_layer(ipt, nbr_filters, (1, 1), weight_decay)
     x_1 = x
 
     filters = nbr_filters
@@ -97,8 +99,8 @@ def create_nasnet(input_shape, nbr_normal_cells, nbr_blocks, weight_decay, nbr_c
             x_1 = x
             x = y
 
-        y, filters = _create_reduction_cell(x, x_1, filters * 2, weight_decay)
-        x_1 = x
+        y, filters = _create_reduction_cell(x, x_1, filters, weight_decay)
+        x_1 = y
         x = y
 
     y = _create_head(x, nbr_classes)
@@ -112,5 +114,5 @@ if __name__ == '__main__':
                           nbr_blocks=2,
                           weight_decay=1e-4,
                           nbr_classes=10,
-                          nbr_filters=5)
-    print(model.print_summary())
+                          nbr_filters=128)
+    print(model.summary())
