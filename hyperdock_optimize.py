8c86d54591c19a6d24abead016e17bf695a1ce5d
Erik Gärtner <erik@gartner.io>
2018-03-11 17:04:17 +0100

    Add hyperdock files

---
+++ hyperdock_optimize.py
@@ -0,0 +1,19 @@
+import json
+
+from train import train_model
+
+
+def read_params():
+    with open('/hyperdock/params.json', 'r') as f:
+        return json.load(f)
+
+
+if __name__ == '__main__':
+
+    params = read_params()
+    params['dump_dir'] = '/hyperdock/out'
+
+    loss = train_model(**params)
+
+    with open('/hyperdock/loss.json', 'w') as f:
+        json.dump(loss, f)
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import os
+import json
 
 from keras.optimizers import *
 from keras.callbacks import *
@@ -19,33 +21,52 @@ def create_callbacks(max_epochs):
             return lr
 
     run_dir = datetime.today().strftime('%Y%m%d-%H%M%S')
-    cbs.append(ReduceLROnPlateau(monitor='val_loss', factor=0.5, verbose=1, min_lr=1e-6, patience=10))
+    cbs.append(ReduceLROnPlateau(monitor='val_loss', factor=0.5, verbose=1,
+                                 min_lr=1e-6, patience=10))
     cbs.append(TensorBoard(log_dir='./logs/%s' % run_dir, batch_size=64))
-    cbs.append(ModelCheckpoint(filepath='./weights/weights_%s_.{epoch:02d}-{val_acc:.2f}.ckpt' % run_dir, verbose=1, period=1, save_best_only=True))
+    cbs.append(ModelCheckpoint(filepath='./weights/weights_%s_.{epoch:02d}-{val_acc:.2f}.ckpt' % run_dir,
+                               verbose=1, period=1, save_best_only=True))
     return cbs
+
+
+def dump_infomation(dump_dir, model):
+    if dump_dir is None:
+        return
+
+    with open(os.path.join(dump_dir, 'model.json'), 'w') as f:
+        json.dump(model.to_json(), f, indent=2)
+
+    with open(os.path.join(dump_dir, 'model.txt'), 'w') as f:
+        f.write(model.summary)
 
 
 def train_model(max_epochs=300, optimizer=SGD(lr=0.1, momentum=0.9, nesterov=True),
                 dense_layers=[6, 12, 24, 16], growth_rate=24,
-                compression=0.5, dropout=0.2, weight_decay=1e-4, batch_size=64):
+                compression=0.5, dropout=0.2, weight_decay=1e-4, batch_size=64,
+                logdir='./logs', weightsdir='./weights', info_dump_dir=None):
 
     start_time = time.time()
-
     (generator_train, generator_test), (x_train, y_train), (x_test, y_test), (x_val, y_val) = load_cifar10()
+
     model = create_densenet(
         input_shape=(32, 32, 3), dense_layers=dense_layers,
         growth_rate=growth_rate, nbr_classes=10, weight_decay=weight_decay,
-        compression=compression, dropout=dropout)
+        compression=compression, dropout=dropout
+    )
 
     cbs = create_callbacks(max_epochs)
-    model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['acc'])
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['acc'])
 
-    history = model.fit_generator(generator_train.flow(x_train, y_train, batch_size=batch_size, seed=0),
-                        callbacks=cbs, epochs=max_epochs,
-                        validation_data=generator_test.flow(x_val, y_val, seed=0),
-                        verbose=1)
+    history = model.fit_generator(
+        generator_train.flow(x_train, y_train, batch_size=batch_size, seed=0),
+        callbacks=cbs, epochs=max_epochs,
+        validation_data=generator_test.flow(x_val, y_val, seed=0),
+        verbose=1
+    )
 
-
+    dump_infomation(dump_dir, model)
     best_val_acc = max(history.history['val_acc'])
     best_acc = max(history.history['acc'])
     return {
