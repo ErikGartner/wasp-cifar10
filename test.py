f95ec82a270fc469596e6bdae096e1e2d5ef33ee
Erik Gartner <gartner@maths.lth.se>
2018-03-13 15:01:22 +0100

    Fix bugs in test script

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import sys
+import time
 
 from keras.models import load_model
+from keras.optimizers import *
 
 from dataset import load_cifar10
 
@@ -14,10 +16,15 @@ def test_model(model_path=None):
      (x_val, y_val)) = load_cifar10()
 
     model = load_model(model_path)
+    optimizer = SGD(lr=0.1, momentum=0.9, nesterov=True)
+    model.compile(optimizer=optimizer,
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['acc'])
 
     loss = model.evaluate_generator(generator_test.flow(x_test, y_test))
     print('Loss was: %s' % loss)
 
+    return loss
 
 if __name__ == '__main__':
-    print(test_model(sys.argv[1]))
+   print(test_model(sys.argv[1]))
