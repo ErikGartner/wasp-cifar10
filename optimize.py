934e920510b2033f17da2b8131b3486023d1af06
Erik Gärtner <erik@gartner.io>
2018-03-13 08:56:41 +0100

    Removed old optimize file

--- optimize.py
+++ optimize.py
@@ -1,41 +1,20 @@
-import pickle
+import json
 
-import hyperopt
-from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
-from hyperopt.pyll.base import scope
+from train import train_model
 
 
-def train_model_hyperopt(args):
-    print('Calling train_model with: %s' % args)
-
-    # Import keras in the training thread
-    from train import train_model
-
-    # Apply all the kwargs to the training function
-    return train_model(**args)
-
-
-def main():
-    space = {
-        'max_epochs': 50,
-        'growth_rate': scope.int(hp.quniform('growth_rate', 12, 48, 12)),
-        'batch_size': hp.choice('batch_size', [64, 84]),
-        'dense_layers': hp.choice('dense_layers',
-            [[6, 12, 24, 14], [6, 12, 32, 24]]),
-        'compression': hp.quniform('compression', 0.4, 0.6, 0.1),
-        'dropout': hp.quniform('dropout', 0, 0.5, 0.25),
-    }
-
-    trials = Trials()
-    best = fmin(train_model_hyperopt,
-                space=space,
-                algo=tpe.suggest,
-                max_evals=5,
-                trials=trials)
-
-    pickle.dump(trials, open('trials_object.p', 'wb'))
-    print(best)
+def read_params():
+    with open('/hyperdock/params.json', 'r') as f:
+        return json.load(f)
 
 
 if __name__ == '__main__':
-    main()
+
+    params = read_params()
+    params['logdir'] = '/hyperdock/out/logs'
+    params['weightsdir'] = '/hyperdock/out'
+
+    loss = train_model(**params)
+
+    with open('/hyperdock/loss.json', 'w') as f:
+        json.dump(loss, f)
