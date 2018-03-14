b71a7033351284066020fa94faa4a21e216d3ddf
Erik Gärtner <erik@gartner.io>
2018-03-14 09:43:39 +0100

    Add automatic model testing

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import sys
 import time
+import os
 
 from keras.models import load_model
 from keras.optimizers import *
@@ -16,15 +17,31 @@ def test_model(model_path=None):
      (x_val, y_val)) = load_cifar10()
 
     model = load_model(model_path)
-    optimizer = SGD(lr=0.1, momentum=0.9, nesterov=True)
-    model.compile(optimizer=optimizer,
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['acc'])
+
+    if not model.built:
+        optimizer = SGD(lr=0.1, momentum=0.9, nesterov=True)
+        model.compile(optimizer=optimizer,
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['acc'])
 
     loss = model.evaluate_generator(generator_test.flow(x_test, y_test))
     print('Loss was: %s' % loss)
-
     return loss
 
+
 if __name__ == '__main__':
-   print(test_model(sys.argv[1]))
+
+    path = sys.argv[1]
+
+    if os.path.isdir(path):
+        print("Testing all models in %s" % path)
+        for model_file in os.listdir(path):
+
+            try:
+                print('Testing %s' % model_file)
+                test_model(os.path.join(path, model_file))
+            except:
+                print('Some error occured!')
+
+    else:
+        test_model(path)
