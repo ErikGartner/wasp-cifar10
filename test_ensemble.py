42ab5c3dac14db2edb8a848e1df2fe5b87a6f899
Erik Gärtner <erik@gartner.io>
2018-03-27 18:15:33 +0200

    Fix many many bugs

--- test_ensemble.py
+++ test_ensemble.py
@@ -13,9 +13,6 @@ from dataset import load_cifar10
 
 def init_model(model_path):
 
-    # Load the dataset with augmentations
-    start_time = time.time()
-
     model = load_model(model_path)
 
     optimizer = SGD(lr=0.1, momentum=0.9, nesterov=True)
@@ -26,19 +23,24 @@ def init_model(model_path):
 
 
 def predict_models(models, x):
-    predictions = list(map(lambda model: model.predict(x), models))
+    predictions = list(map(lambda model:
+                           model.predict_on_batch(x),
+                           models))
     nb_preds = len(x)
     for i, pred in enumerate(predictions):
-        pred = list(map(lambda probas: probas * 1, pred))
-        predictions[i] = np.asarray(pred).reshape(nb_preds, 10, 1)
-    weighted_preds = np.concatenate(predictions, axis=2)
-    weighted_avg = np.mean(weighted_preds, axis=2)
-    votes = np.argmax(weighted_avg, axis=1)
+        pred = list(map(
+            lambda probas: np.argmax(probas, axis=-1), pred
+        ))
+        predictions[i] = np.asarray(pred).reshape(nb_preds, 1)
+    argmax_list = list(np.concatenate(predictions, axis=1))
+    votes = np.asarray(list(
+        map(lambda arr: max(set(arr)), argmax_list)
+    ))
     return votes
 
 
 if __name__ == '__main__':
-    
+
     path = sys.argv[1]
     print("Loading all models in %s" % path)
 
@@ -50,7 +52,6 @@ if __name__ == '__main__':
             model = init_model(os.path.join(path, model_file))
             models.append(model)
 
-            models = models * 2
             break
         except RuntimeError:
             print('Some error occured!')
@@ -60,8 +61,17 @@ if __name__ == '__main__':
      (x_train, y_train), (x_test, y_test),
      (x_val, y_val)) = load_cifar10()
 
-    # predict using ensabmle:
-    y = predict_models(models, x_test)
-    correct = y == y_test
+    # Evaluate the models
+    correct = 0
+    total = 0
+    for x_batch, y_batch in generator_test.flow(x_test, y_test, batch_size=32):
+        print('%d/%d' % (total, len(y_test)))
+        total += len(y_batch)
 
-    print(np.sum(correct) / len(y_test))
+        y = predict_models([model], x_batch)
+        correct += np.sum(y.flatten() == y_batch.flatten())
+
+        if total >= len(y_test):
+            break
+
+    print('Correct: %d/%d (%f)' % (correct, total, correct / total))
